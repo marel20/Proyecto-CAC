68c07954f69075af5a08f58f6f9197e1846c3e4b
marel20 <marelli_11@hotmail.com>
2022-11-17 17:49:39 -0300

    modificaciones disciplinas

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 window.addEventListener('scroll', btnUp)
 
 function btnUp() {
-if(window.scrollY > nav.offsetHeight + 750) {
+    const nav = document.getElementById('nav')
+if(window.scrollY > nav.offsetHeight + 2100) {
     this.document.getElementById('up').style.display = 'block';
 } else {
     document.getElementById('up').style.display = 'none';
@@ -18,3 +19,16 @@ toggle.addEventListener('click', () => {
     links.classList.toggle('active');
 })
 
+//pop-up
+
+const open = document.getElementById('open-card');
+const modalContent = document.getElementById('containerModal');
+const close = document.getElementById('close-card');
+
+open.addEventListener('click', () => {
+    modalContent.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modalContent.classList.remove('show');
+});
