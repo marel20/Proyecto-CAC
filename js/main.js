7a9a1d5be20f769a7f3f5e0884493185ecf89b1b
marel20 <marelli_11@hotmail.com>
2022-11-22 19:50:26 -0300

    agregue galeria al popup

--- js/main.js
+++ js/main.js
@@ -21,14 +21,50 @@ toggle.addEventListener('click', () => {
 
 //pop-up
 
-const open = document.getElementById('open-card1');
+const open1 = document.getElementById('open-card1');
 const modalContent = document.getElementById('containerModal');
 const close = document.getElementById('close-card');
 
-open.addEventListener('click', () => {
+open1.addEventListener('click', () => {
     modalContent.classList.add('show');
 });
 
 close.addEventListener('click', () => {
     modalContent.classList.remove('show');
 });
+
+// const open2 = document.getElementById('open-card2');
+
+// open2.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
+
+// const open3 = document.getElementById('open-card3');
+
+// open3.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
+
+// const open4 = document.getElementById('open-card4');
+
+// open4.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
+
+// const open5 = document.getElementById('open-card5');
+
+// open5.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
+
+// const open6 = document.getElementById('open-card6');
+
+// open6.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
+
+// const open = document.getElementById('open-card7');
+
+// open.addEventListener('click', () => {
+//     modalContent.classList.add('show');
+// });
